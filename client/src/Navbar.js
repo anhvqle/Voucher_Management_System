a698ed441854e29d46ec13b9c08847db018cf266
Anh Le <a.v.le@wustl.edu>
2021-01-26 15:24:53 +0700

    navbar items added

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -2,9 +2,18 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="collapse navbar-collapse container" id="navbarNav">
-                <ul className="navbar-nav">
+                <ul className="navbar-nav nav-fill w-100">
                     <li className="nav-item active">
-                        <a className="nav-link" href="/list">Home</a>
+                        <a className="nav-link" href="/list">Master Voucher</a>
+                    </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Quản lý Đăng ký sử dụng Voucher</a>
+                    </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Quản lý Đại lý</a>
+                    </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Quản lý User</a>
                     </li>
                 </ul>
             </div>
